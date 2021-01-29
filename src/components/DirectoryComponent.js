330f82daa5e45d7c6ff000d4a0f1ab3025a18d53
Kyle Weisse <kyle.weisse@gmail.com>
2021-01-29 01:04:38 -0500

    seperated campsites into its own file, installed two fonts, changed app.css to use those fonts

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,49 +1,43 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: [
-                {
-                    id: 0,
-                    name: 'React Lake Campground',
-                    image: 'assets/images/react-lake.jpg',
-                    elevation: 1233,
-                    description: "Nestled in the foothills of the Chrome Mounatins, blah blah blah puns fly fishers."
-                },
-                {
-                    id: 1,
-                    name: 'Chrome River Campground',
-                    image: 'assets/images/chrome-river.jpg',
-                    elevation: 877,
-                    description: "Spend a few sunny days and nights here IF YOU SURVIVE. "
-                },
-                {
-                    id: 2,
-                    name: 'Breadcramb Trail Campground',
-                    image: 'assets/images/breadcrumb-trail.jpg',
-                    elevation: 2901,
-                    description: /*put fa-music note here. <i class="fas fa-music"></i> */"and always let your Nucamp be your guide."
-                },
-                {
-                    id: 3,
-                    name: 'Redux Woods Campground',
-                    image: 'assets/images/redux-woods.jpg',
-                    elevation: 42,
-                    description: "This area is prone to flooding, leave quickly."
-                }
-            ],
+            selectedCampsite: null
         };
     }
 
+    onCampsiteSelect(campsite) {
+        this.setState({selectedCampsite: campsite});
+    }
+
+    renderSelectedCampsite(campsite) {
+        if (campsite) {
+            return (
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name}/>
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return <div/>;
+    }
+
     render() {
-        const directory = this.state.campsites.map(campsite => {
+        const directory = this.props.campsites.map(campsite => {
             return(
-                <div key={campsite.id} className="col">
-                    <img src={campsite.image} alt={campsite.name} />
-                    <h2> {campsite.name}</h2>
-                    <p>{campsite.description}</p>
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle> {campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
@@ -52,6 +46,11 @@ class Directory extends Component {
             <div className="container">
                 <div className="row">
                     {directory}
+                </div>
+                <div className="row">
+                    <div className="col-md-5 m-1">
+                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
+                    </div>
                 </div>
             </div>
         );

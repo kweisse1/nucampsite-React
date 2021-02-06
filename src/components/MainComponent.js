74c9b2f328c60339994096a5d4a4515ed88f6774
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-06 09:22:51 -0500

    added contact page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
@@ -31,6 +32,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />}/>
+          <Route exact path="/contactus" component={Contact}/>
           <Redirect to ='/home' />
         </Switch>
         <Footer/>

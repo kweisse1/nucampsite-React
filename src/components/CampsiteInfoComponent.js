09a097134e09b928fe6ea197f874a4dcab690d7c
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-08 13:09:21 -0500

    added breadcrumbs to Directory

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderCampsite({campsite}){
+function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name}/>
+                <CardImg top src={campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardTitle>{campsite.name}</CardTitle>
                     <CardText>{campsite.description}</CardText>
@@ -15,36 +16,46 @@ function RenderCampsite({campsite}){
     );
 }
 
-function RenderComments({comments}){
-    if(comments){
-        return(
+function RenderComments({ comments }) {
+    if (comments) {
+        return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-                {comments.map(comment => 
+                {comments.map(comment =>
                     <div key={comment.id}>
-                        <p>{comment.text}<br/>
-                        -- {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                        <p>{comment.text}<br />
+                        -- {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                     </div>)}
             </div>
         )
     }
-    return <div/>
+    return <div />
 }
 
 function CampsiteInfo(props) {
     console.log(props);
-    if (props.campsite){
-        return(
+    if (props.campsite) {
+        return (
             <div className="container">
                 <div className="row">
-                    <RenderCampsite campsite={props.campsite}/>
-                    <RenderComments comments={props.comments}/>
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         )
     }
 
-    return(<div/>);
+    return (<div />);
 }
 
 export default CampsiteInfo;

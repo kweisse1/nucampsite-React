ca35d3464cde6a7f9a89f6c226597fb269588763
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-11 20:47:05 -0500

    react redux beginning

---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+
+export const initialState = {
+    campsties: CAMPSITES,
+    comments: COMMENTS,
+    partners: PARTNERS,
+    promotions: PROMOTIONS
+};
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+};

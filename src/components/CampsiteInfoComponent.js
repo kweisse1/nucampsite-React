eb0d501f8027856b9547bf97fdf3d7b7d212d310
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-16 15:59:36 -0500

    redux actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -41,8 +41,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -106,21 +106,25 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-                {comments.map(comment =>
-                    <div key={comment.id}>
-                        <p>{comment.text}<br />
-                        -- {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                    </div>)}
-                <CommentForm />
+                {comments.map(comment => {
+                    return(
+                        <div key={comment.id}>
+                            <p>{comment.text}<br />
+                            -- {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                            </p>
+                        </div>
+                    );
+                })}
+                <CommentForm campsiteId={campsiteId} addComment={addComment}  />
             </div>
         )
     }
-    return <div />
+    return (<div >render comments broken</div>)
 }
 
 function CampsiteInfo(props) {
@@ -140,7 +144,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments} 
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         )

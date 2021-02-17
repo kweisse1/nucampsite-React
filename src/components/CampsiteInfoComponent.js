1d0cf0c70ac26c081e39b90c792ed06281eb69c9
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-17 15:04:45 -0500

    thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -71,32 +72,32 @@ class CommentForm extends Component {
 
                             <div className="form-group">
                                 <label for="author">Your Name</label>
-                                <Control.text model=".author" name="author" id="author" 
-                                    placeholder="Your Name" className="form-control" 
+                                <Control.text model=".author" name="author" id="author"
+                                    placeholder="Your Name" className="form-control"
                                     validators={{
                                         minLength: minLength(2),
                                         maxLength: maxLength(15)
                                     }}
                                 />
                                 <Errors
-                                        className="text-danger"
-                                        model=".author"
-                                        show="touched"
-                                        component="div"
-                                        messages={{
-                                            minLength: 'Must be at least 2 character',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                    />
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    component="div"
+                                    messages={{
+                                        minLength: 'Must be at least 2 character',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                />
                             </div>
-                            
+
                             <div className="form-group">
                                 <label for="text">Comment</label>
-                                <Control.textarea className="form-control" model=".text" name="text" id="text" placeholder="comment" rows="6"/>
+                                <Control.textarea className="form-control" model=".text" name="text" id="text" placeholder="comment" rows="6" />
                             </div>
 
                             <Button type="submit" color="primary">
-                                        Submit
+                                Submit
                             </Button>
 
                         </LocalForm>
@@ -107,13 +108,13 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
                 {comments.map(comment => {
-                    return(
+                    return (
                         <div key={comment.id}>
                             <p>{comment.text}<br />
                             -- {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
@@ -121,7 +122,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment}  />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         )
     }
@@ -129,6 +130,26 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.ErrMess}</h4>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     console.log(props.comments);
     if (props.campsite) {
         return (
@@ -145,8 +166,8 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
-                        comments={props.comments} 
+                    <RenderComments
+                        comments={props.comments}
                         addComment={props.addComment}
                         campsiteId={props.campsite.id}
                     />

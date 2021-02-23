b342c32ec7c8541734da46b4eff680368feb4aa8
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-23 15:31:19 -0500

    comment array call fixed

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -53,7 +53,6 @@ class Main extends Component {
     } 
 
     const CampsiteWithId = ({match}) => {
-      console.log("MainComponent CamspiteWithId match:", match);
       return (
         <React.Fragment>
           <CampsiteInfo
@@ -61,7 +60,7 @@ class Main extends Component {
             isLoading={this.props.campsites.isLoading}
             errMess={this.props.campsites.errMess}
             campsitesErrMess={this.props.campsites.errMess}
-            comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+            comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
             commentsErrMess={this.props.comments.errMess}
             addComment={this.props.addComment}
           />

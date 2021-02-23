f3bde3f93155c58ac835dda6a6cd77f183b99100
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-23 18:56:40 -0500

    added post to server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -45,7 +45,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         console.log("handle submit values:" + values.text);
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -109,7 +109,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -123,7 +123,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         )
     }
@@ -169,7 +169,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>

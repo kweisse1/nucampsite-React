e9f9bbd75e1607157c8f08ef96b32b76b95cbe48
Kyle Weisse <kyle.weisse@gmail.com>
2021-02-26 00:33:20 -0500

    partner from server

--- src/redux/campsites.js
+++ src/redux/campsites.js
@@ -6,11 +6,10 @@ export const Campsites = (state = {
         campsites: []
     }, action) => {
     switch(action.type) {
+        
         case ActionTypes.ADD_CAMPSITES:
-            console.log('reduxADD_CAMPSITES')
             return {...state, isLoading: false, errMess: null, campsites: action.payload};
         case ActionTypes.CAMPSITES_LOADING:
-            console.log('reduxCAMPSITESLOADING')
             return {...state, isLoading: true, errMess: null, campsites: []}
         case ActionTypes.CAMPSITES_FAILED:
             return {...state, isLoading: false, errMess: action.payload};
